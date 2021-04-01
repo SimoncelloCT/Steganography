658d90b98c7ce0c5397c2045030badc83ed6d679
Simoncello98 <simonescionti@yahoo.com>
2021-04-01 11:21:49 +0200

    Initial Commit

---
+++ Steganography/SteganographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteganographyApp.swift
+//  Steganography
+//
+//  Created by Simone Scionti on 01/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SteganographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
